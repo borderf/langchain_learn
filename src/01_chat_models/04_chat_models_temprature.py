c640d833ef0d623d6f532695d1f8836781dff4fb
borderf <15653771198@163.com>
2025-12-26 22:32:39 +0800

    feat: chat models temperature

---
+++ src/01_chat_models/04_chat_models_temprature.py
@@ -0,0 +1,32 @@
+from langchain.chat_models import init_chat_model
+from dotenv import load_dotenv
+
+load_dotenv()
+
+question = "请写一句关于春天的诗"
+
+print("【保守模式】")
+
+model1 = init_chat_model(
+    model="Qwen/Qwen3-8B",
+    model_provider="openai",
+    # 保守模式
+    temperature=0.0,
+)
+
+for i in range(3):
+    response = model1.invoke(question)
+    print(f"第{i + 1}次：{response.content}")
+
+print("*" * 50)
+
+print("【创意模式】")
+model2 = init_chat_model(
+    model="Qwen/Qwen3-8B",
+    model_provider="openai",
+    # 创意模式
+    temperature=1
+)
+for i in range(3):
+    response = model2.invoke(question)
+    print(f"第{i + 1}次：{response.content}")
