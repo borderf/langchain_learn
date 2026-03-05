c072222c28bb0541849388429c3e7b650d819e1f
borderf <15653771198@163.com>
2026-03-05 22:13:49 +0800

    feat: deep agents demo

---
+++ deepagents/01_deepagents_demo.py
@@ -0,0 +1,27 @@
+from dotenv import load_dotenv
+from deepagents import create_deep_agent
+from langchain.chat_models import init_chat_model
+
+load_dotenv()
+
+model = init_chat_model(
+    model="Qwen/Qwen3-8B",
+    model_provider="openai",
+)
+
+agent = create_deep_agent(
+    model=model,
+)
+
+while True:
+    user_input = input("\n用户消息: ")
+    if user_input.lower() in ["exit", "quit"]:
+        print("结束对话")
+        break
+    response = agent.invoke({
+        "messages": [
+            {"role": "user", "content": user_input}
+        ]
+    })
+    for message in response["messages"]:
+        message.pretty_print()
